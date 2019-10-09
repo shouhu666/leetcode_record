224b3640df46762c189032f4eb5cef3960a6ce6c
孙天宇 <2016011108@secoder.net>
2019-10-09 11:29:53 +0800

    add Palindrome string

---
+++ 回文串/Palindrome.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if  len(s) == 0 :
+            return True
+        length = len(s)
+        index_1, index_2 = 0, length - 1
+        while index_1 < index_2:
+            if not s[index_1].isalnum():
+                index_1 += 1
+                continue
+            if not s[index_2].isalnum():
+                index_2 -= 1
+                continue
+            if s[index_1].lower() != s[index_2].lower():
+                return False
+            elif index_1 == index_2 or index_1 == index_2 - 1:
+                return True
+            else:
+                index_1 += 1
+                index_2 -= 1
+        return True
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+        s1 = ""
+        for i in s:
+            if i.isalnum():
+                s1 += i.lower()
+        s2 = "".join(list(reversed(s1)))
+        if s1 == s2:
+            return True
+        else:
+            return False
